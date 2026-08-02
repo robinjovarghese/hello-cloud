fc807338bc5489b6f3f90a98668df42c0dc7f819
robinjovarghese <robinjovarghese007@gmail.com>
2026-08-02 05:48:11 +0530

    Update Express server to serve index page

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve static files
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
