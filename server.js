cf644bc1a68b4a6e7a394d284952326434e43833
robinjovarghese <robinjovarghese007@gmail.com>
2026-08-02 06:09:01 +0530

    Resolve merge conflict in server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-// Serve static files
+// Serve static files (index.html, css, images, etc.)
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
@@ -15,22 +14,4 @@ const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
-=======
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send(`
-        <h1>Hello Azure 🚀</h1>
-        <h2>Current Server Time</h2>
-        <h3>${new Date()}</h3>
-    `);
-});
-
-const port = process.env.PORT || 8080;
-
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
->>>>>>> e9bffe2d900f8a70edd23b456be7c4710cc5de84
 });
